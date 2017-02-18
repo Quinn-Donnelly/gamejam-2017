cfc2a9f2633a049d3a7eb67740c22d337e501df1
Logan Granberry <logan.granberry@gmail.com>
2017-02-18 16:44:29 -0600

    added point cloud camera

---
+++ Assets/PointCloudDisplay.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointCloudDisplay : MonoBehaviour {
+
+    public RenderTexture texture;
+    MeshFilter filter;
+    public float pointSize = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+        filter = GetComponent<MeshFilter>();
+        Mesh mesh = filter.mesh;
+        List<Vector3> vertices = new List<Vector3>();
+        List<Vector2> texcoords = new List<Vector2>();
+        int[] indices = new int[texture.width * texture.height * 4 + 2];
+        int i = 0;
+        float offset = (1.0f / texture.width) / 2f;
+        for (int y = 0; y < texture.height; y++)
+        {
+            for (int x = 0; x < texture.width; x++)
+            {
+                vertices.Add(new Vector3( pointSize,  pointSize, 0));
+                vertices.Add(new Vector3( pointSize, -pointSize, 0));
+                vertices.Add(new Vector3(-pointSize, -pointSize, 0));
+                vertices.Add(new Vector3(-pointSize,  pointSize, 0));
+                texcoords.Add(new Vector2(x / (float)texture.width + offset, y / (float)texture.height + offset));
+                texcoords.Add(new Vector2(x / (float)texture.width + offset, y / (float)texture.height + offset));
+                texcoords.Add(new Vector2(x / (float)texture.width + offset, y / (float)texture.height + offset));
+                texcoords.Add(new Vector2(x / (float)texture.width + offset, y / (float)texture.height + offset));
+                indices[i] = i;
+                indices[i + 1] = i + 1;
+                indices[i + 2] = i + 2;
+                indices[i + 3] = i + 3;
+                i += 4;
+            }
+        }
+
+        //vertices.Add(new Vector3(float.MaxValue, float.MaxValue, float.MaxValue));
+        //vertices.Add(new Vector3(-float.MaxValue, -float.MaxValue, -float.MaxValue));
+        //texcoords.Add(new Vector2(0,0));
+        //texcoords.Add(new Vector2(0,0));
+        //indices[i] = i;
+        //indices[i + 1] = i + 1;
+
+        mesh.SetVertices(vertices);
+        mesh.SetUVs(0, texcoords);
+        mesh.SetIndices(indices, MeshTopology.Quads, 0);
+        mesh.bounds = new Bounds(Vector3.zero, Vector3.one * 99999);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
---
+++ Assets/Shaders/PointShader.shader
@@ -0,0 +1,56 @@
+﻿Shader "PointCloud/pointMesh" {
+	Properties
+	{
+		_MainTex("Texture", 2D) = "white" {}
+		_Texture2("color", 2D) = "white" {}
+	}
+	SubShader{
+		Pass{
+
+		CGPROGRAM
+
+		#pragma vertex vert
+		#pragma fragment frag
+		#pragma glsl
+		#include "UnityCG.cginc"
+		sampler2D _MainTex;
+		float4 _MainTex_ST;
+
+		sampler2D _Texture2;
+		float4 _Texture2_ST;
+		struct v2f {
+			float4 pos : SV_POSITION;
+			float2 texcoord : TEXCOORD0;
+			fixed3 color : COLOR0;
+			float size : PSIZE;
+			float2 depth : TEXCOORD1;
+		};
+
+		v2f vert(appdata_base v)
+		{
+			v2f o;
+			float4 tex = tex2Dlod(_MainTex, float4(v.texcoord.xy, 0, 0));
+			if (tex.a < 0.01) {
+				tex = float4(-9999, -9999, -9999, 1);
+			}
+			o.pos = UnityObjectToClipPos(tex);
+			float4 quadPos = mul(UNITY_MATRIX_P, v.vertex);
+			o.pos += quadPos;
+			o.size = 10;
+			o.color = float3(1, 1, 1);
+			o.texcoord = v.texcoord;
+			COMPUTE_EYEDEPTH(o.depth);
+			return o;
+		}
+
+		fixed4 frag(v2f i) : SV_Target
+		{
+			//UNITY_OUTPUT_DEPTH(i.depth);
+			return fixed4(tex2D(_Texture2, i.texcoord));
+		}
+			ENDCG
+
+		}
+	}
+		FallBack "Diffuse"
+}
