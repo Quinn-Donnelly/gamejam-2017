682467cd9eb6f72e345c48bcf1922d5e47b62b61
Nicholas Mahony <nicholas.mahony@eagles.usm.edu>
2017-02-18 01:57:21 -0600

    Object prefabs and scripts v1

---
+++ Assets/Scripts/ObjectScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScript : MonoBehaviour {
+    public GameObject thing;
+    public Transform currentLocation;
+    public Transform targetLocation;
+    public float speed = 0.5f;
+    public bool isLoop;
+    public bool isPatrol;
+    public Transform[] targets;
+    public int targetIterator = 0;
+
+	// Use this for initialization
+	void Start () {
+        currentLocation = thing.transform;
+        targetLocation = targets[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentLocation = thing.transform;
+        MoveTo(targetLocation);
+	}
+
+    void MoveTo(Transform target)
+    {
+        float step = speed * Time.deltaTime;
+        thing.transform.position = Vector3.MoveTowards(currentLocation.position, targetLocation.position, step);
+        if (Vector3.Distance(thing.transform.position, target.position) < 1)
+        {
+            if (isLoop)
+            {
+                if (targetIterator >= targets.Length - 1)
+                {
+                    targetIterator = 0;
+                }
+                else
+                {
+                    targetIterator++;
+                }
+                targetLocation = targets[targetIterator];
+            }
+            else if (isPatrol)
+            {
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SteamPipeScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteamPipeScript : MonoBehaviour {
+    public GameObject steam;
+    public AudioSource sound;
+    public bool isOn;
+    public float toggleRate = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		isOn = true;
+        sound = GetComponent<AudioSource>();
+        StartCoroutine(SteamLoop());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator SteamLoop()
+    {
+        while (enabled)
+        {
+            if (isOn)
+            {
+                isOn = false;
+                sound.Stop();
+                GetComponent<MeshRenderer>().enabled = false;
+            }
+            else
+            {
+                isOn = true;
+                sound.Play();
+                GetComponent<MeshRenderer>().enabled = true;
+            }
+            yield return new WaitForSeconds(toggleRate);
+        }
+    }
+}
