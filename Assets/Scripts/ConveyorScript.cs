2fab22b5dff07d80f18dd89d922915d82f9fedec
Nicholas Mahony <nicholas.mahony@eagles.usm.edu>
2017-02-18 05:04:26 -0600

    assets update includes sliding stone noise loop

---
+++ Assets/Scripts/ConveyorScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorScript : MonoBehaviour {
+    public GameObject conveyorBelt;
+    public float power = 0.15f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("test");
+        if (other.attachedRigidbody)
+        {
+            other.attachedRigidbody.AddForce(conveyorBelt.transform.forward * power, ForceMode.Force);
+        }
+    }
+
+}
---
+++ Assets/Scripts/FireVentScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireVentScript : MonoBehaviour {
+    public GameObject fire;
+    public AudioSource sound;
+    public bool isOn;
+    public float flareRate = 2.0f;
+
+    private bool active;
+    private MeshRenderer fireMesh;
+
+    // Use this for initialization
+    void Start()
+    {
+        isOn = true;
+        active = true;
+        sound = GetComponent<AudioSource>();
+        fireMesh = this.gameObject.transform.GetChild(0).gameObject.GetComponent<MeshRenderer>();
+        StartCoroutine(SteamLoop());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Pressed()
+    {
+        active = !active;
+        isOn = false;
+        sound.Stop();
+        fireMesh.enabled = false;
+    }
+
+    IEnumerator SteamLoop()
+    {
+        while (enabled)
+        {
+            if (active)
+            {
+                if (isOn)
+                {
+                    isOn = false;
+                    sound.Stop();
+                    fireMesh.enabled = false;
+                }
+                else
+                {
+                    isOn = true;
+                    sound.Play();
+                    fireMesh.enabled = true;
+                }
+            }
+            yield return new WaitForSeconds(flareRate);
+        }
+    }
+}
